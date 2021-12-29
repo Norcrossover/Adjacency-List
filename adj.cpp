28090c41fb42facf4ca343876d6b52ec2ab9a8f8
Kobe <kobenorcross@gmail.com>
2021-12-28 21:14:32 -0800

    finished the ajacency list, need to do other fxns within it now

--- adj.cpp
+++ adj.cpp
@@ -1,53 +1,77 @@
 #include "adj.h"
 
 
-AdjacencyList:AdjacencyList() : size(0), list(nullptr);
+
+AdjacencyList::AdjacencyList() : lists(nullptr), size(0) {}
 
 
 AdjacencyList::AdjacencyList(const char * fileName) {
 	ifstream input;
-	EdgeList tempLists[1000000000];
+	//this->size = getSizeOfGraph(input, fileName);
+	this->size = 10;
+	if (size == -1) exit(-1);
+	//this->lists = new EdgeList[size];
+	if (!extractData(input, fileName)) {
+		std::cout << "failed to extract" << std::endl;
+		exit(-1);
+	}
+}
+
+
+
+int AdjacencyList::getSizeOfGraph(ifstream & input, const char * fileName) {
+	int size = 0;
 	input.open(fileName);
+	if (input.good()) {
+		while (input.peek()!=EOF) {
+			if (input.peek() == '\n') size++;
+		}
+		input.close();
+		return size;
+	}
+	return -1;
+}
+
+
+
+bool AdjacencyList::extractData(ifstream & input, const char * fileName) {
+	this->lists = new EdgeList[this->size];
+	input.open("test2.txt");
 	if (input.good()) {
 		while (input.peek() != EOF) {
 			int count = 0, val = 0;
-			EdgeList * newList = new EdgeList();
-			while (input.peek != '\n') {
+			char buffer; 
+			while (input.peek() != '\n') {
 				input >> val;
-				input.get();
-				newList->addNode(val);
+				input.get(buffer);
+				if (buffer == '\n') break; 
+				this->lists[count].addNode(val);
 			}
-			tempLists[count] = newList;
 			count++;
 		}
-		this->size = count+1;
-		this->lists = new EdgeList[size];
-		lists = tempLists;
+		input.close();
+		return true;
 	}
-	else {
-		exit();
-	}
+	return false;
 }
 
 
 
 AdjacencyList::~AdjacencyList() {
-	for (int i = 0; i < size; i++) {
-		delete list[i].head;
-	}
-	delete [] list;
+	delete [] lists;
 	size = 0;
 }
 
 
-void AdjacencyList::depthFirstTraversal(int v) {
+void AdjacencyList::depthFirstTraversal() {
 	return;
 }
 
 
-
+/*
 void AdjacencyList::operator=(const EdgeList *& newList) {
 	for (int i = 0; i < size; i++) {
 		this->list[i]->head = newList->getHead();
 	}
 }
+*/
--- adj.h
+++ adj.h
@@ -1,13 +1,16 @@
 #include "edge.h"
+using namespace std;
 
 class AdjacencyList {
 	public:
 		AdjacencyList();
 		AdjacencyList(const char * fileName);
+		int getSizeOfGraph(ifstream & input, const char * fileName);
+		bool extractData(ifstream & input, const char * fileName);
 		~AdjacencyList();
-		void operator=(const EdgeList *& newList);
-		void depthFirstTraversal(int v);
+//		void operator=(const EdgeList *& newList);
+		void depthFirstTraversal();
 	private:
-		EdgeList lists[];
+		EdgeList * lists;
 		int size;
 };
